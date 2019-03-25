39ea8e61860c5e187d2135a32afae9fcb2d35cbc
kvsrohit <kvsrohit@yahoo.com>
2019-03-25 11:39:23 +0530

    version fix

--- etherium/contracts/inbox.sol
+++ etherium/contracts/inbox.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
 	string public message;
-	constructor(string initialMessage) public {
+	function Inbox(string initialMessage) public {
 		message = initialMessage;
 	}
 	function setMessage(string newMessage) public {
--- etherium/test/inbox.test.js
+++ etherium/test/inbox.test.js
@@ -7,18 +7,19 @@ const web3 = new Web3(provider);
 const {interface, bytecode} = require('../compile');
 let accounts;
 let inbox;
-beforeEach( async () => {
-  accounts = await web3.eth.getAccounts();
-
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: '0x'+bytecode, arguments:['Hi There!']})
-    .send({from: accounts[0], gas: '1000000'});
-  inbox.setProvider(provider);
-
-});
 
 describe('Inbox', () => {
+  beforeEach( async () => {
+    accounts = await web3.eth.getAccounts();
+  
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({data: bytecode, arguments:['Hi There!']})
+      .send({from: accounts[1], gas: '1000000'});
+    
+    inbox.setProvider(provider);
+  });
+  
   it('deploys a contract', () => {
-    console.log(inbox);
+    console.log(inbox.options.address);
   });
 });
